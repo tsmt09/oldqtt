a42f1a4363129ce2ecd2e1802ceb234a2d6f2102
Tobias Schmitt <tschmittldk@gmail.com>
2024-12-31 17:08:48 +0100

    sending and receiving mqtt messages

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# OldQTT
+
+This projects aims at testing the egui library by providing a nice-to-use MQTT client which is less about modern-webdesign with big buttons etc, but more works as good-old software.
+
+
 # eframe template
 
 [![dependency status](https://deps.rs/repo/github/emilk/eframe_template/status.svg)](https://deps.rs/repo/github/emilk/eframe_template)
@@ -19,8 +24,6 @@ Change the name of the crate: Choose a good name for your project, and change th
     * Change the `package.authors`
 * `main.rs`
     * Change `eframe_template::TemplateApp` to `your_crate::TemplateApp`
-* `index.html`
-    * Change the `<title>eframe template</title>` to `<title>your_crate</title>`. optional.
 * `assets/sw.js`
   * Change the `'./eframe_template.js'` to `./your_crate.js` (in `filesToCache` array)
   * Change the `'./eframe_template_bg.wasm'` to `./your_crate_bg.wasm` (in `filesToCache` array)
@@ -47,38 +50,6 @@ On Linux you need to first run:
 On Fedora Rawhide you need to run:
 
 `dnf install clang clang-devel clang-tools-extra libxkbcommon-devel pkg-config openssl-devel libxcb-devel gtk3-devel atk fontconfig-devel`
-
-### Web Locally
-
-You can compile your app to [WASM](https://en.wikipedia.org/wiki/WebAssembly) and publish it as a web page.
-
-We use [Trunk](https://trunkrs.dev/) to build for web target.
-1. Install the required target with `rustup target add wasm32-unknown-unknown`.
-2. Install Trunk with `cargo install --locked trunk`.
-3. Run `trunk serve` to build and serve on `http://127.0.0.1:8080`. Trunk will rebuild automatically if you edit the project.
-4. Open `http://127.0.0.1:8080/index.html#dev` in a browser. See the warning below.
-
-> `assets/sw.js` script will try to cache our app, and loads the cached version when it cannot connect to server allowing your app to work offline (like PWA).
-> appending `#dev` to `index.html` will skip this caching, allowing us to load the latest builds during development.
-
-### Web Deploy
-1. Just run `trunk build --release`.
-2. It will generate a `dist` directory as a "static html" website
-3. Upload the `dist` directory to any of the numerous free hosting websites including [GitHub Pages](https://docs.github.com/en/free-pro-team@latest/github/working-with-github-pages/configuring-a-publishing-source-for-your-github-pages-site).
-4. we already provide a workflow that auto-deploys our app to GitHub pages if you enable it.
-> To enable Github Pages, you need to go to Repository -> Settings -> Pages -> Source -> set to `gh-pages` branch and `/` (root).
->
-> If `gh-pages` is not available in `Source`, just create and push a branch called `gh-pages` and it should be available.
->
-> If you renamed the `main` branch to something else (say you re-initialized the repository with `master` as the initial branch), be sure to edit the github workflows `.github/workflows/pages.yml` file to reflect the change
-> ```yml
-> on:
->   push:
->     branches:
->       - <branch name>
-> ```
-
-You can test the template app at <https://emilk.github.io/eframe_template/>.
 
 ## Updating egui
 
--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,41 @@
+use std::{sync::mpsc::Receiver, thread::JoinHandle, time::Duration};
+
+use egui_extras::{Column, TableBuilder};
+use rumqttc::{Client, Event, Incoming, MqttOptions};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+    host: String,
+    port: String,
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+    send_topic: String,
+    #[serde(skip)]
+    send_payload: String,
+
+    #[serde(skip)]
+    mqtt_client: Option<Client>,
+    #[serde(skip)]
+    mqtt_jh: Option<JoinHandle<()>>,
+    #[serde(skip)]
+    mqtt_receiver: Option<Receiver<Event>>,
+    #[serde(skip)]
+    incoming: Vec<Event>,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            host: "".to_owned(),
+            port: "".to_owned(),
+            send_payload: "".to_owned(),
+            send_topic: "".to_owned(),
+            mqtt_client: None,
+            mqtt_jh: None,
+            mqtt_receiver: None,
+            incoming: vec![],
         }
     }
 }
@@ -58,7 +79,7 @@ impl eframe::App for TemplateApp {
                             ctx.send_viewport_cmd(egui::ViewportCommand::Close);
                         }
                     });
-                    ui.add_space(16.0);
+                    ui.add_space(0.0);
                 }
 
                 egui::widgets::global_theme_preference_buttons(ui);
@@ -67,28 +88,134 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            let heading = if self.mqtt_client.is_none() {
+                "Connect to MQTT"
+            } else {
+                "MQTT Connected!"
+            };
+            ui.heading(heading);
 
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
+                ui.label("Host: ");
+                ui.text_edit_singleline(&mut self.host);
             });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
+            ui.horizontal(|ui| {
+                ui.label("Port: ");
+                ui.text_edit_singleline(&mut self.port);
+                if self.mqtt_jh.is_none() && ui.button("connect").clicked() {
+                    let port = self.port.parse::<u16>().unwrap_or(1883);
+                    let rand = fastrand::u16(0..u16::MAX);
+                    let options =
+                        MqttOptions::new(format!("oldqtt_{}", rand), self.host.as_str(), port);
+                    log::debug!("clicked connect");
+                    let (client, mut connection) = Client::new(options, 20);
+                    self.mqtt_client = Some(client);
+                    log::debug!("Connected to {}", self.host);
+                    let (tx, rx) = std::sync::mpsc::channel::<rumqttc::Event>();
+                    let ctx_c = ctx.clone();
+                    self.mqtt_jh = Some(std::thread::spawn(move || {
+                        // loop over notifications
+                        for notification in connection.iter() {
+                            match notification {
+                                Err(error) => {
+                                    log::error!("MQTT Error: {:?}", error);
+                                    return ();
+                                }
+                                Ok(event) => {
+                                    if let Err(e) = tx.send(event) {
+                                        log::error!("Channel Error: {}", e);
+                                    };
+                                    ctx_c.request_repaint_after(Duration::from_millis(10));
+                                }
+                            }
+                        }
+                    }));
+                    self.mqtt_receiver = Some(rx);
+                    self.mqtt_client
+                        .as_ref()
+                        .unwrap()
+                        .subscribe("#", rumqttc::QoS::ExactlyOnce)
+                        .unwrap();
+                } else if self.mqtt_jh.is_some() {
+                    if ui.button("disconnect").clicked() {
+                        let handle = self.mqtt_jh.take();
+                        let client = self.mqtt_client.take();
+                        if let (Some(client), Some(handle)) = (client, handle) {
+                            client.disconnect().unwrap();
+                            handle.join().unwrap();
+                            self.incoming.clear();
+                        }
+                    }
+                }
+            });
+            if let Some(receiver) = &self.mqtt_receiver {
+                while let Some(event) = receiver.try_recv().ok() {
+                    if let Event::Incoming(_) = event {
+                        log::debug!("Add event to table: {:?}", event);
+                        self.incoming.push(event);
+                    }
+                }
             }
 
             ui.separator();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+            if self.mqtt_jh.is_some() {
+                ui.horizontal(|ui| {
+                    ui.label("Topic: ");
+                    ui.text_edit_singleline(&mut self.send_topic);
+                    ui.label("Payload: ");
+                    ui.text_edit_singleline(&mut self.send_payload);
+                    if ui.button("send").clicked() {
+                        if let Some(client) = &self.mqtt_client {
+                            client
+                                .publish(
+                                    &self.send_topic,
+                                    rumqttc::QoS::ExactlyOnce,
+                                    false,
+                                    self.send_payload.as_str(),
+                                )
+                                .unwrap();
+                        }
+                    }
+                });
+                TableBuilder::new(ui)
+                    .column(Column::auto().at_least(50.0).resizable(true))
+                    .column(Column::remainder())
+                    .striped(true)
+                    .resizable(true)
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.heading("Topic");
+                        });
+                        header.col(|ui| {
+                            ui.heading("Payload");
+                        });
+                    })
+                    .body(|mut body| {
+                        for event in &self.incoming {
+                            if let Event::Incoming(Incoming::Publish(publish)) = event {
+                                let topic = publish.topic.clone();
+                                let payload = String::from_utf8(publish.payload.to_vec()).unwrap();
+                                body.row(30.0, |mut row| {
+                                    row.col(|ui| {
+                                        ui.label(topic);
+                                    });
+                                    row.col(|ui| {
+                                        ui.label(payload);
+                                    });
+                                });
+                            }
+                        }
+                    });
+            }
+        });
 
+        egui::TopBottomPanel::bottom("footer").show(ctx, |ui| {
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
+                ui.separator();
             });
         });
     }
